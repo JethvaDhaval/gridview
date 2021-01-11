9f3a63b262b2e623b54a35765750e5270053235c
JethvaDhaval <dcjethva2525@gail.com>
2021-01-11 20:29:42 +0530

    gridview of getthisapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+import React, { useState } from 'react'
+
+import android from './android.png'
+import ios from './ios.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="preview">
+          <div className="container">
+            <div className="row mt-5">
+              <div className="col">
+                <div style={{width: "100px", height: "100px", backgroundColor: "red", margin: "0 auto 0 auto"}}></div>
+              </div>
+            </div>
+            <div className="row mt-2">
+              <div className="col text-center">
+                <h4>Get This App Here</h4>
+              </div>
+              </div>
+            <div className="row mt-2">
+              <div className="col-sm m-1 p-0">
+                <button type="button" className="btn btn-dark p-0" style={{width: "100%", backgroundColor: "black", minHeight: "39px"}}><h6 style={{textTransform: "uppercase"}}>Wise</h6><h6 style={{fontSize: "12px",lineHeight: "0.2"}}>Use on computer</h6></button>
+              </div>
+              <div className="col-sm m-1 p-0">
+                <button type="button" className="btn btn-dark w-100 p-0" >
+                <img src={android} width="100%" style={{borderRadius: "2px", maxHeight: "50px"}} />
+                </button>
+              </div>
+              <div className="col-sm m-1 p-0">
+                <button type="button" className="btn btn-dark w-100 p-0">
+                   <img src={ios} width="100%" style={{borderRadius: "2px", maxHeight: "50px"}} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
